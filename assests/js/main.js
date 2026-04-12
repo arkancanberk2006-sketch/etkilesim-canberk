4902d3b1a02a568b0f64c5e1bd9a82aeca40d19c
Canberkarkan <arkancanberk.2006@gmail.com>
2026-04-12 14:57:47 +0300

    main.js

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,1 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+}
